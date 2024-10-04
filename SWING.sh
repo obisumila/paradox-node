885c8dd50fc915bff1bb38cd0615686f6931fe2e
obisumila <obisumila@gmail.com>
2024-10-05 05:31:25 +0800

    Add files via upload

---
+++ SWING.sh
@@ -0,0 +1,79 @@
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S goto -dm nohup ./start.sh > start.log 2>&1 &
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S goto -dm nohup ./start.sh > start.log 2>&1 &
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S goto -dm nohup ./start.sh > start.log 2>&1 &
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S goto -dm nohup ./start.sh > start.log 2>&1 &
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S goto -dm nohup ./start.sh > start.log 2>&1 &
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S goto -dm nohup ./start.sh > start.log 2>&1 &
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S goto -dm nohup ./start.sh > start.log 2>&1 &
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S goto -dm nohup ./start.sh > start.log 2>&1 &
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S goto -dm nohup ./start.sh > start.log 2>&1 &
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S goto -dm nohup ./start.sh > start.log 2>&1 &
+screen -ls
+sleep 2
+clear
+screen -ls ; 
